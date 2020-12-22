1ada179e01f5bb0f4d028d9af06b9e8abf9e079f
swizkon <jonas@jerndin.se>
2020-12-22 20:23:54 +0100

    Test react

--- src/pages/goteborg.js
+++ src/pages/goteborg.js
@@ -4,11 +4,23 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const getStuff = () => {
+  return "Stuff";
+}
+
+const hairDressers = () => {
+
+  return (<h1>Frisörer</h1>);
+}
+
+
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
+    { getStuff() }
     <h1>G&ouml;teborg</h1>
     <p>Welcome to page 2</p>
+    { hairDressers() }
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,31 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const links = () => {
+
+  const salonger = [
+    { name: "Göteborg", link: "/goteborg/" },
+    { name: "Kinna", link: "/kinna/" },
+  ]
+
+  return (
+    <div>
+      { salonger.map(s => (
+        <Link to={s.link}> {s.name} </Link>
+      )) }
+    </div>
+  )
+}
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <SEO title="kolla kolla" />
+    <h1>Välj salong</h1>
+    {links()}
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/goteborg/">G&ouml;teborg</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
